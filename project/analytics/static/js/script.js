f54e6c97738bc6b14dc9fdc119204b8a1d2a5f22
Yaroglek <jkl7589@gmail.com>
2024-01-23 09:41:03 +0500

    Commit №11 Исправлена темная тема, снова (переключалась обратно только на той страницу, где была включена). Вместо почти одинаковых файлов под каждую страницу был сделан шаблон base.html, на который вставляется уникальный контент с других страниц.

--- project/analytics/static/js/script.js
+++ project/analytics/static/js/script.js
@@ -1,7 +1,24 @@
-let page = document.querySelector('.page');
-let themeButton = document.querySelector('.theme-button');
+var togButton = document.getElementById("togButton");
 
-themeButton.onclick = function() {
-  page.classList.toggle('light-theme');
-  page.classList.toggle('dark-theme');
-};
+darkOn = localStorage.getItem("dark") == "true" ? true : false;
+setTheme();
+
+function setTheme(){
+    localStorage.setItem("dark", darkOn ? "true" : "false");
+    if(darkOn){
+        document.body.classList.remove("light-theme");
+        document.body.classList.add("dark-theme");
+    }
+    else{
+        document.body.classList.remove("dark-theme");
+        document.body.classList.add("light-theme");
+    }
+}
+
+var darkOn = false;
+function toggle(){
+    darkOn = !darkOn;
+    setTheme();
+}
+
+togButton.addEventListener("click", toggle);
